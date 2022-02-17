6c63fff5c43b628cbce0021249ae0139be9a15a1
Prabhat Ranjan <Prabhat0206@hotmail.com>
2022-02-17 13:18:58 +0530

    settings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## BEZEN API
 
+# BASE URL: https://prabhat02060.pythonanywhere.com/
+
 # documentation link -> https://docs.google.com/document/d/1fCIr2RsXIb9aBkStLVOEQISuvqBxKSUHQuFrxHxwp6c/edit?usp=sharing
--- bezen/urls.py
+++ bezen/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RegisterView, GetUserRecords, AddRecord, UpdateRecord
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,5 +11,5 @@ urlpatterns = [
     path('records', GetUserRecords.as_view(), name='records'),
     path('add', AddRecord.as_view(), name='add'),
     path('update_record/<int:id>', UpdateRecord.as_view(), name='update_record')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
